b30c487c39f3029bf3d5e445639a2fe9a4b14124
hfukuyama <h.i.d.e.20070621@gmail.com>
2023-07-08 08:20:39 +0900

    Finished firebase-react-tutorial

--- src/App.js
+++ src/App.js
@@ -1,23 +1,35 @@
-import logo from './logo.svg';
+import { useEffect, useState } from 'react';
 import './App.css';
+import db from "./firebase";
+import { collection, getDocs, orderBy, onSnapshot, query } from "firebase/firestore"
 
 function App() {
+  const [posts, setPosts] = useState([]);
+
+  useEffect(() => {
+    //データベースからデータを取得。
+    const postData = collection(db, "posts");
+    const q = query(postData, orderBy("timestamp", "desc"));
+    getDocs(q).then((snapShot) => {
+      // console.log(snapShot.docs.map((doc) => ({ ...doc.data() })));
+      setPosts (snapShot.docs.map((doc) => ({ ...doc.data() })));
+    });
+    /* リアルタイムで取得 */
+    onSnapshot(q, (querySnapshot) => {
+      setPosts(querySnapshot.docs.map((doc) => ({...doc.data()})))
+    })
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div>
+        {posts.map((post) => (
+          <div key={post.title}>
+            <h1>{post.title}</h1>
+            <p>{post.text}</p>
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
